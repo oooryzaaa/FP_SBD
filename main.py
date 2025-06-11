2180738c28d32ea991dd07e7a0b2224c109b49a8
oooryzaaa <oryzaqiara1@gmail.com>
2025-06-11 08:56:24 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 from auth import register_user, login_user
 
+def menu_customer(user_id):
+    print(f"\n[Customer Menu] (User ID: {user_id})")
+    
+
+def menu_seller(user_id):
+    print(f"\n[Seller Menu] (User ID: {user_id})")
+    
+
+def menu_admin(user_id):
+    print(f"\n[Admin Menu] (User ID: {user_id})")
+    
+
 while True:
     print("\n=== Sistem E-Commerce ===")
     print("1. Register")
@@ -10,13 +22,25 @@ while True:
     if choice == "1":
         username = input("Username: ")
         password = input("Password: ")
-        role = input("Role (customer/seller): ")
+        role = input("Role (buyer/seller/admin): ").lower()
+        if role not in ['buyer', 'seller', 'admin']:
+            print("Role tidak valid.")
+            continue
         register_user(username, password, role)
 
     elif choice == "2":
         username = input("Username: ")
         password = input("Password: ")
-        login_user(username, password)
+        user_id, role = login_user(username, password)
+        if user_id:
+            if role == "buyer":
+                menu_customer(user_id)
+            elif role == "seller":
+                menu_seller(user_id)
+            elif role == "admin":
+                menu_admin(user_id)
+            else:
+                print("Role tidak dikenali.")
 
     elif choice == "0":
         break
