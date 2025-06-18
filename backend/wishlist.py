4f6f0a9162cb110e70b74ab4bfe79b8cbee043d3
oooryzaaa <oryzaqiara1@gmail.com>
2025-06-18 13:36:10 +0700

    Create wishlist.py

---
+++ backend/wishlist.py
@@ -0,0 +1,12 @@
+class WishlistManager:
+    # [CUSTOMER] Menambahkan produk ke wishlist
+    def add_to_wishlist(self, customer_id, product_id):
+        pass
+
+    # [CUSTOMER] Melihat daftar wishlist pengguna
+    def get_wishlist(self, customer_id):
+        pass
+
+    # [CUSTOMER] Menghapus produk dari wishlist
+    def remove_from_wishlist(self, customer_id, product_id):
+        pass
