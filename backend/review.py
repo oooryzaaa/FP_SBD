a79e9ea766a47c5b5fbc09d8c0f42f5ce6fa6c65
oooryzaaa <oryzaqiara1@gmail.com>
2025-06-18 14:11:13 +0700

    Create review.py

---
+++ backend/review.py
@@ -0,0 +1,12 @@
+class ReviewSystem:
+    # [CUSTOMER] Memberi ulasan untuk produk yang dibeli
+    def add_review(self, customer_id, product_id, rating, review_text):
+        pass
+
+    # [CUSTOMER] Menampilkan semua ulasan dari satu produk
+    def get_reviews(self, product_id):
+        pass
+
+    # [CUSTOMER] Melaporkan ulasan yang tidak pantas
+    def report_review(self, review_id):
+        pass
