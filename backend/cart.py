b88362e14179c407e744979422b488f09b345178
oooryzaaa <oryzaqiara1@gmail.com>
2025-06-18 14:10:52 +0700

    Create cart.py

---
+++ backend/cart.py
@@ -0,0 +1,16 @@
+class ShoppingCart:
+    # [CUSTOMER] Tambahkan produk ke keranjang
+    def add_to_cart(self, customer_id, product_id, quantity):
+        pass
+
+    # [CUSTOMER] Melihat isi keranjang belanja
+    def view_cart(self, customer_id):
+        pass
+
+    # [CUSTOMER] Menghapus produk dari keranjang
+    def remove_from_cart(self, customer_id, product_id):
+        pass
+
+    # [CUSTOMER] Checkout produk yang ada di keranjang
+    def checkout(self, customer_id, payment_info):
+        pass
