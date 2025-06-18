4cdccbe38ffcb4456184e46241fa5f3d66b0aa80
oooryzaaa <oryzaqiara1@gmail.com>
2025-06-18 14:13:02 +0700

    Create category.py

---
+++ backend/category.py
@@ -0,0 +1,12 @@
+class CategoryManager:
+    # [ADMIN / SYSTEM] Menambahkan kategori baru untuk produk
+    def add_category(self, name):
+        pass
+
+    # [ALL ROLES] Melihat semua kategori yang tersedia
+    def get_all_categories(self):
+        pass
+
+    # [ADMIN] Menghapus kategori produk
+    def remove_category(self, name):
+        pass
