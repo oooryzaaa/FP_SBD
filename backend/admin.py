52d9bcb4245a1a86ad8658f5ece369950d264236
oooryzaaa <oryzaqiara1@gmail.com>
2025-06-18 14:11:31 +0700

    Create admin.py

---
+++ backend/admin.py
@@ -0,0 +1,22 @@
+class AdminPanel:
+    # [ADMIN] Melihat semua review yang dilaporkan
+    def view_reported_reviews(self):
+        pass
+
+    # [ADMIN] Menghapus review yang dilaporkan
+    def remove_review(self, review_id):
+        pass
+
+    # [ADMIN] Melihat dan mengelola customer yang dilaporkan
+    def view_reported_customers(self):
+        pass
+
+    def remove_customer(self, customer_id):
+        pass
+
+    # [ADMIN] Melihat dan mengelola seller yang dilaporkan
+    def view_reported_sellers(self):
+        pass
+
+    def remove_seller(self, seller_id):
+        pass
