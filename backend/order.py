421bd0041b75ce2970e17b0f08baab7bc8f86c7d
oooryzaaa <oryzaqiara1@gmail.com>
2025-06-18 14:13:19 +0700

    Create order.py

---
+++ backend/order.py
@@ -0,0 +1,12 @@
+class OrderManager:
+    # [CUSTOMER] Buat pesanan berdasarkan keranjang
+    def create_order(self, customer_id, cart_items):
+        pass
+
+    # [CUSTOMER] Menampilkan riwayat pemesanan
+    def get_order_history(self, customer_id):
+        pass
+
+    # [SELLER] Menampilkan pesanan yang harus dipenuhi
+    def get_orders_for_seller(self, seller_id):
+        pass
