dd3ebbeb9e9c9a622266e86f34f5839679381f26
oooryzaaa <oryzaqiara1@gmail.com>
2025-06-18 14:14:12 +0700

    Create analytics.py

---
+++ backend/analytics.py
@@ -0,0 +1,8 @@
+class SalesAnalytics:
+    # [SELLER] Laporan penjualan seller
+    def get_sales_report(self, seller_id):
+        pass
+
+    # [SYSTEM] Menampilkan produk terlaris (opsional)
+    def get_top_products(self):
+        pass
