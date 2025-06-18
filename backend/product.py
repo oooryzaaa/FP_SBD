e9124ec8690669588365cdf7961e6d5737c855b7
oooryzaaa <oryzaqiara1@gmail.com>
2025-06-18 13:35:53 +0700

    Create product.py

---
+++ backend/product.py
@@ -0,0 +1,24 @@
+class ProductCatalog:
+    # [SELLER] Menambahkan produk baru ke dalam katalog
+    def add_product(self, seller_id, product_data):
+        pass
+
+    # [SELLER] Mengubah informasi produk yang sudah ada
+    def edit_product(self, product_id, new_data):
+        pass
+
+    # [SELLER] Menghapus produk dari katalog
+    def remove_product(self, product_id):
+        pass
+
+    # [CUSTOMER] Menampilkan semua produk untuk dibrowsing
+    def get_all_products(self):
+        pass
+
+    # [ALL ROLES] Mengambil detail satu produk
+    def get_product_by_id(self, product_id):
+        pass
+
+    # [CUSTOMER] Cari produk berdasarkan kategori
+    def search_by_category(self, category_name):
+        pass
