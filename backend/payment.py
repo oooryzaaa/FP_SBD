86c9ce6733303b8f3d28dfb38dc579e0c2e15774
oooryzaaa <oryzaqiara1@gmail.com>
2025-06-18 14:13:45 +0700

    Update payment.py

--- backend/payment.py
+++ backend/payment.py
@@ -1,1 +1,9 @@
+class PaymentSystem:
+    # [CUSTOMER] Proses pembayaran dari customer
+    def process_payment(self, order_id, payment_data):
+        pass
+
+    # [SELLER / SYSTEM] Update status pengiriman
+    def manage_shipment(self, order_id, shipment_data):
+        pass
 
